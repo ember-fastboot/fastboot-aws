748ea7dea4192952116f84b04685d589756a6408
Tom Dale <tom@tomdale.net>
2016-04-20 09:32:49 -0400

    Replace SQS with S3 poll

--- server.js
+++ server.js
@@ -2,13 +2,12 @@
 
 const ExpressHTTPServer = require('./lib/express-http-server');
 const S3Downloader      = require('./lib/s3-downloader');
-const SQSNotifier       = require('./lib/sqs-notifier');
+const S3Notifier        = require('./lib/s3-notifier');
 const UI                = require('./lib/ui');
 const FastBootAppServer = require('./lib/fastboot-app-server');
 
 const S3_BUCKET = process.env.FASTBOOT_S3_BUCKET;
 const S3_KEY    = process.env.FASTBOOT_S3_KEY;
-const SQS_QUEUE = process.env.FASTBOOT_SQS_QUEUE;
 const USERNAME  = process.env.FASTBOOT_USERNAME;
 const PASSWORD  = process.env.FASTBOOT_PASSWORD;
 
@@ -18,8 +17,9 @@ let downloader = new S3Downloader({
   ui: UI
 });
 
-let notifier = new SQSNotifier({
-  queue: SQS_QUEUE,
+let notifier = new S3Notifier({
+  bucket: S3_BUCKET,
+  key: S3_KEY,
   ui: UI
 });
 
