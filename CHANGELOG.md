7b8d3b91ef262517885ab4bea3174f705a9ac64a
Tom Dale <tom@tomdale.net>
2016-05-18 10:59:54 -0400

    Adds Redis caching support

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## CHANGELOG
+
+### 1.0.0-beta.2
+
+* Adds support for Redis caching
--- server.js
+++ server.js
@@ -2,12 +2,16 @@
 
 const S3Downloader      = require('fastboot-s3-downloader');
 const S3Notifier        = require('fastboot-s3-notifier');
+const RedisCache        = require('fastboot-redis-cache');
 const FastBootAppServer = require('fastboot-app-server');
 
-const S3_BUCKET = process.env.FASTBOOT_S3_BUCKET;
-const S3_KEY    = process.env.FASTBOOT_S3_KEY;
-const USERNAME  = process.env.FASTBOOT_USERNAME;
-const PASSWORD  = process.env.FASTBOOT_PASSWORD;
+const S3_BUCKET    = process.env.FASTBOOT_S3_BUCKET;
+const S3_KEY       = process.env.FASTBOOT_S3_KEY;
+const REDIS_HOST   = process.env.FASTBOOT_REDIS_HOST;
+const REDIS_PORT   = process.env.FASTBOOT_REDIS_PORT;
+const REDIS_EXPIRY = process.env.FASTBOOT_REDIS_EXPIRY;
+const USERNAME     = process.env.FASTBOOT_USERNAME;
+const PASSWORD     = process.env.FASTBOOT_PASSWORD;
 
 let downloader = new S3Downloader({
   bucket: S3_BUCKET,
@@ -19,9 +23,21 @@ let notifier = new S3Notifier({
   key: S3_KEY
 });
 
+let cache;
+if (REDIS_HOST || REDIS_PORT) {
+  cache = new RedisCache({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    expiration: REDIS_EXPIRY
+  });
+} else {
+  console.log('No FASTBOOT_REDIS_HOST or FASTBOOT_REDIS_PORT provided; caching is disabled.');
+}
+
 let server = new FastBootAppServer({
   downloader: downloader,
-  notifier: notifier
+  notifier: notifier,
+  cache: cache
 });
 
 server.start();
