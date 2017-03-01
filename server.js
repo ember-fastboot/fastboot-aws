09a6e5def66210ce5bf172755a31874c4f0ddb7a
simon <ihmig@kaliber5.de>
2017-03-01 12:59:51 +0100

    Added beforeMiddleware to force https

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const S3Downloader      = require('fastboot-s3-downloader');
 const S3Notifier        = require('fastboot-s3-notifier');
 const RedisCache        = require('fastboot-redis-cache');
 const FastBootAppServer = require('fastboot-app-server');
+const forceHttps        = require('./lib/force-https');
 
 const S3_BUCKET    = process.env.FASTBOOT_S3_BUCKET;
 const S3_KEY       = process.env.FASTBOOT_S3_KEY;
 const REDIS_HOST   = process.env.FASTBOOT_REDIS_HOST;
 const REDIS_PORT   = process.env.FASTBOOT_REDIS_PORT;
 const REDIS_EXPIRY = process.env.FASTBOOT_REDIS_EXPIRY;
+const FORCE_HTTPS  = process.env.FORCE_HTTPS;
 const USERNAME     = process.env.FASTBOOT_USERNAME;
 const PASSWORD     = process.env.FASTBOOT_PASSWORD;
 
@@ -37,7 +39,12 @@ if (REDIS_HOST || REDIS_PORT) {
 let server = new FastBootAppServer({
   downloader: downloader,
   notifier: notifier,
-  cache: cache
+  cache: cache,
+  beforeMiddleware(app) {
+    if (FORCE_HTTPS) {
+      app.use(forceHttps);
+    }
+  }
 });
 
 server.start();
